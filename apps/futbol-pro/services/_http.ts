e1d57fe8de9abcc69270f7427beec3ecbeaf8472
lkavila <lkavila@uninorte.edu.co>
2021-11-27 16:04:25 -0500

    change dependencies

--- apps/futbol-pro/services/_http.ts
+++ apps/futbol-pro/services/_http.ts
@@ -1,5 +1,5 @@
 const endpoint = "https://v3.football.api-sports.io"
-const apiKey = 'bd0397a068387ba720950752acc2de02'
+const apiKey = '97c029e5e159b72655f3d1795cfde225'
 console.log(process.env.NX_SPORTS_API_KEY)
 export const _get = async (ruta: string) => {
   return await fetch(endpoint + ruta, {
